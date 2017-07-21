f8f959fa0ea0b780e88f730dd6e75e377a29173b
Deepak Kumar vellingiri <aspiring.us@gmail.com>
2017-07-21 16:39:45 +0530

    Adding Classnames module

---
+++ components/AvatarSC.jsx
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+
+const AvatarSc = (props) => {
+  return (
+    <div>AvatarSc Content</div>
+  );
+};
+
+AvatarSc.propTypes = {
+  propName: PropTypes.string
+};
+
+export default AvatarSc;
