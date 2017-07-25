aaa2f6868c4c630fea68e534d82fd2a65bb96659
Deepak Kumar vellingiri <aspiring.us@gmail.com>
2017-07-25 22:25:55 +0530

    Adding PropCheck to Badge

--- components/Badge.jsx
+++ components/Badge.jsx
@@ -1,16 +1,35 @@
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const Badge = styled('div')`
+const Badge = styled.div`
+  color: ${props => props.color};
+  background-color: ${props => props.bg};
   margin-left: 4px;
   margin-right: 4px;
   padding: 4px;
   font-size: 12px;
-  color: #fff;
-  background-color: #0077cc;
   font-weight: 700;
   display: inline-block;
   vertical-align: middle;
   border-radius: 4px;
 `;
+
+/* Props Check */
+Badge.propTypes = {
+  /**
+   * Text Color
+  */
+  color: PropTypes.string,
+  /**
+   * Background Color
+  */
+  bg: PropTypes.string
+};
+
+/* Deafult Props */
+Badge.defaultProps = {
+  color: "#fff",
+  bg: "#000"
+};
 
 export default Badge;
