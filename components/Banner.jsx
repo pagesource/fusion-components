f2ce44eb2b49a65bf700e79f4a92f2f648ba52a6
Deepak Kumar vellingiri <aspiring.us@gmail.com>
2017-07-25 22:28:45 +0530

    Adding PropCheck to Banner

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Banner = styled.div`
@@ -8,7 +9,20 @@ const Banner = styled.div`
   min-height: 80vh;
   background-size: cover;
   background-position: center;
-  background-image: url(${props => props.picUrl || 'https://images.unsplash.com/photo-1462331940025-496dfbfc7564?w=2048&q=20' });
+  background-image: url(${ props => props.picUrl });
 `;
+
+/* Props Check */
+Banner.propTypes = {
+  /**
+   * Image URL
+  */
+  picUrl: PropTypes.string
+};
+
+/* Deafult Props */
+Banner.defaultProps = {
+  picUrl: "https://images.unsplash.com/photo-1462331940025-496dfbfc7564?w=2048&q=20"
+};
 
 export default Banner;
