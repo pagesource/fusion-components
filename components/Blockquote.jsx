9951518f63d02dd81bf62f97691093491ffe7602
Deepak Kumar vellingiri <aspiring.us@gmail.com>
2017-07-25 22:33:21 +0530

    Adding Propcheck to Blockquote

--- components/Blockquote.jsx
+++ components/Blockquote.jsx
@@ -1,8 +1,23 @@
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 const Blockquote = styled.blockquote`
   color: indigo;
   font-size: 20px;
+  text-align: ${props => props.align}
 `;
+
+/* Props Check */
+Blockquote.propTypes = {
+  /**
+   * Text Align
+  */
+  align: PropTypes.string
+};
+
+/* Deafult Props */
+Blockquote.defaultProps = {
+  aling: "left"
+};
 
 export default Blockquote;
