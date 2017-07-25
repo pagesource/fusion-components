489595099fb18644a90b4a5a198cc43b1b00f588
Deepak Kumar vellingiri <aspiring.us@gmail.com>
2017-07-25 16:22:59 +0530

    Adding Blockquote component

---
+++ components/Blockquote.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+const Blockquote = styled.blockquote`
+  color: indigo;
+  font-size: 20px;
+`;
+
+export default Blockquote;
--- index.js
+++ index.js
@@ -9,3 +9,4 @@ export { default as BackgroundImage } from './components/BackgroundImage';
 export { default as Badge } from './components/Badge';
 export { default as Banner } from './components/Banner';
 export { default as Link } from './components/Link';
+export { default as Blockquote } from './components/Blockquote';
