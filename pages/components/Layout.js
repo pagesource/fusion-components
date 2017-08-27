c491c0583de9ffb20d76d05b2da6278416e5f375
vinci Rufus <vinci.rufus@gmail.com>
2017-08-27 07:28:42 +0530

    styling the kitchen-sink page wip

--- pages/components/Layout.js
+++ pages/components/Layout.js
@@ -1,30 +1,32 @@
-import {css} from 'emotion'
-import Header from './Header'
-import SideBar from './Sidebar'
+import { css } from "emotion";
+import styled from "emotion/react";
+import Header from "./Header";
+import SideBar from "./Sidebar";
 
-const layoutContainer = {
-  margin: 20,
-  padding: 20,
+const layoutContainer = css`
+  margin: 20;
+  padding: 20;
   border: '1px solid #DDD'
-}
+`;
 
-const content=css`
+const FixedHeader = styled(Header)`
+position:fixed;
+`;
+
+const content = css`
 width:95%;
 margin-left:25%;
 padding:1rem;
 background:#f9f9f9;
-`
+`;
 
-
-
-const Layout = (props) => (
-  <div className="layoutContainer">
-    <Header />
-    <SideBar/>
-<div className={content}>
-    {props.children}
+const Layout = props =>
+  <div className={layoutContainer}>
+    <FixedHeader />
+    <SideBar />
+    <div className={content}>
+      {props.children}
     </div>
-  </div>
-)
+  </div>;
 
-export default Layout
+export default Layout;
