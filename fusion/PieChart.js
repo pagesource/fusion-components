cb70577a0352816e4b637c925ac25e9eceff496d
ASTRO\V!KAS <vikas10145.12@bitmesra.ac.in>
2017-08-28 18:05:29 +0530

    Added chart component

---
+++ fusion/PieChart.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Pie} from 'react-chartjs-2';
+
+const data = {
+	labels: [
+		'Red',
+		'Green',
+		'Yellow'
+	],
+	datasets: [{
+		data: [300, 50, 100],
+		backgroundColor: [
+		'#FF6384',
+		'#36A2EB',
+		'#FFCE56'
+		],
+		hoverBackgroundColor: [
+		'#FF6384',
+		'#36A2EB',
+		'#FFCE56'
+		]
+	}]
+};
+
+export default React.createClass({
+  displayName: 'PieChart',
+
+  render() {
+    return (
+      <div>
+        <Pie data={data} />
+      </div>
+    );
+  }
+});
--- pages/kitchen-sink.js
+++ pages/kitchen-sink.js
@@ -34,11 +34,33 @@ import CardGrid from "../fusion/CardGrid";
 import Stepper from "../fusion/Stepper";
 import ControlStepper from "../fusion/StepperControl";
 import Pagination from "../fusion/Pagination";
+import DynamicDoughnut from "../fusion/dynamicDoughnut";
+import Doughnut from "../fusion/Doughnut";
+import PieChart from "../fusion/PieChart";
 
 const KitchenSink = props =>
   <Layout>
     <ThemeProvider theme={theme}>
       <div>
+      <h2>Pie Chart</h2>
+        <div>
+          <PieChart />
+        </div>
+        <Row />
+        <hr />
+       <h2>Doughnut</h2>
+        <div>
+          <Doughnut />
+        </div>
+        <Row />
+        <hr />
+      <h2>DynamicDoughnut</h2>
+        <div>
+          <DynamicDoughnut />
+        </div>
+        <Row />
+        <hr />
+        
         <h2>Pagination</h2>
         <div>
           <Pagination />
