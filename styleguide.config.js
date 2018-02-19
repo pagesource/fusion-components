2caacc1057a33828a21209d38c533b4fce067165
Vishweshwaran Suri <v5.itsme@gmail.com>
2018-02-19 15:07:41 -0500

    final update before approving PR

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,30 +1,30 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   sections: [
     {
-      name: "Introduction",
-      content: "lib/styleguide/introduction.md",
+      name: 'Introduction',
+      content: 'lib/styleguide/introduction.md',
       sections: [
         {
-          name: "Installation",
-          content: "lib/styleguide/installation.md",
-          description: "The description for the installation section"
-        }
-      ]
+          name: 'Installation',
+          content: 'lib/styleguide/installation.md',
+          description: 'The description for the installation section',
+        },
+      ],
     },
     {
-      name: "Fusion Components",
-      components: "fusion/**/*.js"
-    }
+      name: 'Fusion Components',
+      components: 'fusion/**/*.js',
+    },
   ],
   ignore: [
-    "**/Link 2.js",
-    "**/Column.js",
-    "**/Column 2.js",
-    "**/EmailSubscribe.js",
-    "**/Row.js",
-    "**/Row 2.js"
+    '**/Link 2.js',
+    '**/Column.js',
+    '**/Column 2.js',
+    '**/EmailSubscribe.js',
+    '**/Row.js',
+    '**/Row 2.js',
   ],
   webpackConfig: {
     module: {
@@ -33,35 +33,35 @@ module.exports = {
           test: /\.jsx?$/,
           exclude: /node_modules/,
           include: [
-            path.resolve(__dirname, "fusion"),
-            path.resolve(__dirname, "docs"),
-            path.resolve(__dirname, "lib", "styleguide")
+            path.resolve(__dirname, 'fusion'),
+            path.resolve(__dirname, 'docs'),
+            path.resolve(__dirname, 'lib', 'styleguide'),
           ],
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["es2015", "react"],
-            plugins: ["transform-object-rest-spread"],
-            babelrc: false
-        }
-        }
-      ]
-    }
+            presets: ['es2015', 'react'],
+            plugins: ['transform-object-rest-spread'],
+            babelrc: false,
+          },
+        },
+      ],
+    },
   },
   defaultExample: true,
   showCode: true,
   editorConfig: {
-    lineNumbers: false
+    lineNumbers: false,
   },
-  styleguideDir: path.resolve(__dirname, "docs"),
+  styleguideDir: path.resolve(__dirname, 'docs'),
   getExampleFilename(componentPath) {
     return componentPath
       .replace(
         path.dirname(componentPath),
         `${__dirname}/lib/styleguide/components`
       )
-      .replace(path.extname(componentPath), ".md");
+      .replace(path.extname(componentPath), '.md');
   },
   styleguideComponents: {
-    Wrapper: path.join(__dirname, "/lib/styleguide/wrapper")
-  }
+    Wrapper: path.join(__dirname, '/lib/styleguide/wrapper'),
+  },
 };
