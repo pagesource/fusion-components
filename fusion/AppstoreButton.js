0dcc9f7d89c72b8c2d7119e454e01583674abbc7
Shouvik Bhuiyan <shouvik.bhuiyan@sapient.com>
2018-06-29 12:22:04 +0530

    Refactoring this to a purecomponent as navigator object is not accessible otherwise

--- fusion/AppstoreButton.js
+++ fusion/AppstoreButton.js
@@ -1,66 +1,117 @@
 import { css } from 'emotion';
 import PropTypes from 'prop-types';
-import React from 'react';
-import { withTheme } from 'theming';
+import React, { PureComponent } from 'react';
 
-const AppstoreButton = ({
-  iosStoreClick,
-  androidStoreClick,
-  appleImage,
-  androidImage,
-  gap,
-}) => {
-  const AppleLogo = css`
-    width: 100%;
-    max-width: 160px;
-    height: auto;
-    cursor: pointer;
-    outline: none;
-    margin-bottom: ${gap}px;
-    @media (min-width: 768px) {
-      margin-bottom: 0;
-      margin-right: ${gap}px;
+export default class AppstoreButton extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.getMobileOperatingSystemIphone = this.getMobileOperatingSystemIphone.bind(
+      this
+    );
+    this.getMobileOperatingSystemAndroid = this.getMobileOperatingSystemAndroid.bind(
+      this
+    );
+    this.AppleLogo = css`
+      width: 100%;
+      max-width: 160px;
+      height: auto;
+      cursor: pointer;
+      outline: none;
+      margin-bottom: ${this.props.gap}px;
+      @media (min-width: 768px) {
+        margin-bottom: 0;
+        margin-right: ${this.props.gap}px;
+      }
+    `;
+    this.AndroidLogo = css`
+      width: 100%;
+      max-width: 160px;
+      height: auto;
+      cursor: pointer;
+      outline: none;
+    `;
+    this.parent = css`
+      display: flex;
+      @media (min-width: 320px) {
+        flex-direction: column;
+      }
+      @media (min-width: 768px) {
+        flex-direction: row;
+      }
+    `;
+    this.hide = css`
+      display: none;
+    `;
+  }
+
+  componentDidMount() {
+    /*
+      * check for the browser type
+    */
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    if (/android/i.test(userAgent)) {
+      this.getMobileOperatingSystemIphone('android');
+      this.getMobileOperatingSystemAndroid('android');
     }
-  `;
-  const AndroidLogo = css`
-    width: 100%;
-    max-width: 160px;
-    height: auto;
-    cursor: pointer;
-    outline: none;
-  `;
-  const parent = css`
-    display: flex;
-    @media (min-width: 320px) {
-      flex-direction: column;
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+      this.getMobileOperatingSystemIphone('apple');
+      this.getMobileOperatingSystemAndroid('apple');
     }
-    @media (min-width: 768px) {
-      flex-direction: row;
+  }
+
+  getMobileOperatingSystemIphone(agent = '') {
+    /*
+      * hide the icon when the browser is of android device
+      * show the icon in all other devices and laptop/desktop
+    */
+    if (agent.toLowerCase() === 'android') {
+      return this.hide;
     }
-  `;
-  return (
-    <div className={parent}>
-      <div
-        className={AppleLogo}
-        onKeyPress={() => {}}
-        role="button"
-        tabIndex={0}
-        onClick={iosStoreClick}
-      >
-        <img className={AppleLogo} src={appleImage} alt="apple" />
+    return this.AppleLogo;
+  }
+
+  getMobileOperatingSystemAndroid(agent = '') {
+    /*
+      * hide the icon when the browser is of apple device
+      * show the icon in all other devices and laptop/desktop
+    */
+    if (agent.toLowerCase() === 'android') {
+      return this.hide;
+    }
+    return this.AndroidLogo;
+  }
+
+  render() {
+    const {
+      iosStoreClick,
+      appleImage,
+      androidStoreClick,
+      androidImage,
+    } = this.props;
+    return (
+      <div className={this.parent}>
+        <div
+          className={this.getMobileOperatingSystemIphone()}
+          onKeyPress={() => {}}
+          role="button"
+          tabIndex={0}
+          onClick={iosStoreClick}
+        >
+          <img className={this.AppleLogo} src={appleImage} alt="apple" />
+        </div>
+        <div
+          className={this.getMobileOperatingSystemAndroid()}
+          onKeyPress={() => {}}
+          role="button"
+          tabIndex={0}
+          onClick={androidStoreClick}
+        >
+          <img className={this.AndroidLogo} src={androidImage} alt="android" />
+        </div>
       </div>
-      <div
-        className={AndroidLogo}
-        onKeyPress={() => {}}
-        role="button"
-        tabIndex={0}
-        onClick={androidStoreClick}
-      >
-        <img className={AndroidLogo} src={androidImage} alt="android" />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 AppstoreButton.propTypes = {
   iosStoreClick: PropTypes.func.isRequired,
@@ -71,9 +122,12 @@ AppstoreButton.propTypes = {
 };
 
 AppstoreButton.defaultProps = {
+  /*
+    * gap signifies the gap between two icons
+    * when in mobile the gap is vertical
+    * when in large devices (e.g. desktop), the gap is horizontal
+  */
   appleImage: 'http://i68.tinypic.com/161nu2u.png',
   androidImage: 'http://i66.tinypic.com/dylrbo.jpg',
   gap: '3',
 };
-
-export default withTheme(AppstoreButton);
